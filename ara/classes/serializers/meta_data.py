fad8e38bae1ca1c1414bc408a78a6d8395c17c66
Kang-in Kim <talkwithraon@gmail.com>
2019-11-06 03:20:20 +0900

    Simple is the best

--- ara/classes/serializers/meta_data.py
+++ ara/classes/serializers/meta_data.py
@@ -2,13 +2,6 @@ from rest_framework import serializers
 
 
 class MetaDataModelSerializer(serializers.ModelSerializer):
-    def get_extra_kwargs(self):
-        read_only_fields = list(getattr(self.Meta, 'read_only_fields', []))
-
-        for meta_data_field in ('created_at', 'updated_at'):
-            if meta_data_field not in read_only_fields:
-                read_only_fields.append(meta_data_field)
-
-        setattr(self.Meta, 'read_only_fields', tuple(read_only_fields))
-
-        return super().get_extra_kwargs()
+    created_at = serializers.ReadOnlyField()
+    updated_at = serializers.ReadOnlyField()
+    deleted_at = serializers.ReadOnlyField()
